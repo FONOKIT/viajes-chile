58025305fa4fb6b16cb61258088235dcb3fd0674
FONOKIT <fonokit.2024@gmail.com>
2024-07-01 11:15:27 -0400

    modificacion script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,18 @@
-<script>
-    $(document).ready(function(){
-        // Smooth scroll for links
-        $('a.nav-link').on('click', function(event) {
-            if (this.hash !== "") {
-                event.preventDefault();
+$(document).ready(function() {
+    // Smooth scrolling for nav links
+    $('a.nav-link').on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
 
-                var hash = this.hash;
-
-                $('html, body').animate({
-                    scrollTop: $(hash).offset().top
-                }, 800, function(){
-                    window.location.hash = hash;
-                });
-            }
-        });
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
     });
-</script>
+
+    // Tooltip initialization
+    $('[data-bs-toggle="tooltip"]').tooltip();
+});
